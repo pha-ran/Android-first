812b96af1487c4b206a5ff3dc91157644196662c
pha-ran <pha-ran@naver.com>
2021-02-23 18:16:30 +0900

    Add Project Ex_KakaoLogin

---
+++ Ex_KakaoLogin/app/src/main/java/com/example/ex_kakaologin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ex_kakaologin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
