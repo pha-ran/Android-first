0f02873d3950c3f653bdc9a8da632888b24e4315
pha-ran <pha-ran@naver.com>
2021-05-26 09:31:55 +0900

    ActivityMainBinding

--- View_Binding/app/src/main/java/com/example/view_binding/MainActivity.kt
+++ View_Binding/app/src/main/java/com/example/view_binding/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.view_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.view_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "view binding"
     }
 }
